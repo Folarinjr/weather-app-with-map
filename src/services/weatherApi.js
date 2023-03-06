f32b6ad7d802aaac2af74ba48184d46f06ff92ac
Folarinjr <folarinademola2@gmail.com>
2023-03-06 15:06:01 +0100

    Build bug fixed

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -5,7 +5,7 @@ const baseUrl = `https://weatherapi-com.p.rapidapi.com/`;
 // const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
 const weatherHeaders = {
-  // "X-RapidAPI-Key": apiKey,
+  // "X-RapidAPI-Key": import.meta.env.VITE_REACT_WEATHER_API_KEY,
   "X-RapidAPI-Key": "9f4c576137mshd157a6b0231e6c8p1fe999jsnaac72a3c03d0",
   "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
 };
